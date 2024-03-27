014eec8fca7fcd5b566eb94c69222e9e5b9a7267
Daniel Segev <danielsegev87@gmail.com>
2024-03-27 12:43:40 +0200

    9_commit

--- run.py
+++ run.py
@@ -14,6 +14,25 @@ import players_stats
 import play_trivia
 import main_game
 
+
+csv_drive_path_generator()
+import_data()
+select_random_player()
+clue_1()
+clue_2()
+clue_3()
+clue_4()
+clue_5()
+clue_6()
+clue_7()
+clue_8()
+players_stats()
+general_stats()
+play_game()
+choose_first()
+choose_next()
+
+
 # Main function
 def main():
     choose_first()
